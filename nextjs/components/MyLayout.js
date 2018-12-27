7d8b8012e31212bdca5f0326cb546deb99b0704f
Matt Levine <matt.levine@blueriver.com>
2018-12-26 23:49:13 -0800

    Adding example Mura module React component

--- nextjs/components/MyLayout.js
+++ nextjs/components/MyLayout.js
@@ -3,10 +3,10 @@ import Header from './Header'
 const layoutStyle = {}
 
 const Layout = (props) => (
-  <div style={layoutStyle}>
-    <Header {...props}/>
-    {props.children}
-  </div>
+	<div style={layoutStyle}>
+		<Header {...props}/>
+		{props.children}
+	</div>
 )
 
 export default Layout
